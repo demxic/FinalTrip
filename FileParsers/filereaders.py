7bdf885910f3879ceb471d8779f15777496fffe6
demxic <demxic@gmail.com>
2019-02-07 22:39:21 -0600

    feat : Read in each pbs file and clean all unwanted data

--- FileParsers/filereaders.py
+++ FileParsers/filereaders.py
@@ -1,3 +1,5 @@
+from data.regex import page_number_RE
+
 def verify_files(data_folder: str, file_names: list) -> list:
     """
     Verify files existence in data_folder
@@ -12,3 +14,19 @@ def verify_files(data_folder: str, file_names: list) -> list:
         else:
             print("file {} not found!".format(file_name))
     return files
+
+
+def page_number_remover(file: str) -> str:
+    """
+    Remove =======pagenumber======= string from trips.txt file
+
+    When using PDF Mate converter tu turn each .pdf file into .txt, the app will insert
+    such long string with page numbers. This must be removed in order to read all trips
+    correctly
+
+    """
+    with open(file, 'r') as f:
+        content = f.read()
+        content_new = page_number_RE.sub(repl='', string=content)
+    return content_new
+
